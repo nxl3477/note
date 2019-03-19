29119a088f44a8bd34d5b1db75458fc28b392fc4
nxl3477gmailcom <nxl3477@gmail.com>
2019-03-20 00:44:08 +0800

    inversify 架构师第二讲

--- Javascript/inversify框架/code/ioc/ioc.ts
+++ Javascript/inversify框架/code/ioc/ioc.ts
@@ -1,8 +1,16 @@
 import { Container, inject } from 'inversify'
 import { controller, interfaces, httpGet } from 'inversify-koa-utils';
-import { provide, buildProviderModule } from "inversify-binding-decorators";
+import { provide, buildProviderModule, fluentProvide } from "inversify-binding-decorators";
 import TAGS from '../constant/tags'
 import Router from 'koa-router'
+
+// 当用到再导出的 流式 provide
+let provideThrowable = function () {
+  return fluentProvide(identifier)
+    .whenTargetNamed(name)
+    .done()
+}
+
 
 export {
   Container,
@@ -12,5 +20,6 @@ export {
   Router,
   inject,
   TAGS,
-  provide
+  provide,
+  provideThrowable
 }
